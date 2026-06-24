0a66b7777543b98e6eb1545084a672b35a27de16
Efrain Antonio Silva Nocentino <efsilva-@c3r1s6.42urduliz.com>
2026-06-24 12:00:13 +0200

    V.02

---
+++ includes/Cub3d.h
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efsilva- <efsilva-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/15 13:01:48 by efsilva-          #+#    #+#             */
+/*   Updated: 2026/06/22 10:45:30 by efsilva-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "libft.h"
+# include "../mlx/minilibx-linux/mlx.h"
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <math.h>
+
+/* ── ERRORS ── */
+# define ERR_ARGS    "Usage: ./cub3D <map.cub>\n"
+# define ERR_EXT     "Map must have .cub extension\n"
+# define ERR_OPEN    "Could not open file\n"
+# define ERR_MALLOC  "Memory allocation failed\n"
+# define ERR_TEXTURE "Invalid or missing texture path\n"
+# define ERR_COLOR   "Invalid color format\n"
+# define ERR_MAP     "Invalid map\n"
+# define ERR_PLAYER  "Map must have exactly one player\n"
+# define ERR_CLOSE   "Map is not closed by walls\n"
+# define ERR_CHAR    "Invalid character in map\n"
+
+/* ── STRUCTS ── */
+typedef struct s_img
+{
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+	int		width;
+	int		height;
+}	t_img;
+
+typedef struct s_cub
+{
+	char	*no_texture;
+	char	*so_texture;
+	char	*we_texture;
+	char	*ea_texture;
+	int		floor_rgb[3];
+	int		ceil_rgb[3];
+	char	**map;
+	int		map_height;
+	int		map_width;
+	double	player_x;
+	double	player_y;
+	char	player_dir;
+	void	*mlx;
+	void	*win;
+	t_img	img;
+}	t_cub;
+
+/* ************************************************************************** */
+/*                            PARSER FUNCTIONS                                */
+/* ************************************************************************** */
+
+/* parser/parse_config.c */
+void	parse_config(t_cub *cub, int fd);
+
+/* parser/parse_map.c */
+void	parse_map(t_cub *cub, int fd);
+
+/* parser/valid_map.c */
+void	valid_map(t_cub *cub);
+
+/* ************************************************************************** */
+/*                             UTILS FUNCTIONS                                */
+/* ************************************************************************** */
+
+/* parser/utils_parser.c */
+char	*read_line(int fd);
+void	ft_error(t_cub *cub, char *msg);
+void	free_cub(t_cub *cub);
+void	free_map(t_cub *cub);
+
+/* parser/utils_map.c */
+int		is_valid_char(char c);
+int		is_player(char c);
+char	*skip_spaces(char *line);
+
+/* ************************************************************************** */
+/*                              INIT FUNCTIONS                                */
+/* ************************************************************************** */
+
+/* game/init.c */
+void	init_cub(t_cub *cub);
+
+#endif
