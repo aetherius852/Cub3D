71f357862dd8111e4af3bf1bd8a40f376f7452a6
Efrain Antonio Silva Nocentino <efsilva-@c3r1s6.42urduliz.com>
2026-06-24 12:04:00 +0200

    V.02

---
+++ libft/ft_split.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efsilva- <efsilva-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 17:23:53 by efsilva-          #+#    #+#             */
+/*   Updated: 2024/12/17 12:37:47 by efsilva-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_countword(char const *s, char c)
+{
+	int	countwords;
+
+	countwords = 0;
+	while (*s != '\0')
+	{
+		while (*s == c)
+			s++;
+		if (*s != '\0')
+			countwords++;
+		while (*s != '\0' && *s != c)
+			s++;
+	}
+	return (countwords);
+}
+
+static char	*ft_get_next_word(char const *s, char c, int *start, int *len)
+{
+	int	i;
+
+	while (s[*start] == c)
+		(*start)++;
+	i = *start;
+	while (s[i] && s[i] != c)
+		i++;
+	*len = i - *start;
+	return (ft_substr(s, *start, *len));
+}
+
+static void	ft_free_result(char **result, int count)
+{
+	int	i;
+
+	i = 0;
+	while (i < count)
+	{
+		free(result[i]);
+		i++;
+	}
+	free(result);
+}
+
+static char	**ft_fill_result(char **result, char const *s, char c, int words)
+{
+	int		i;
+	int		start;
+	int		len;
+	char	*next_word;
+
+	i = 0;
+	start = 0;
+	while (i < words)
+	{
+		next_word = ft_get_next_word(s, c, &start, &len);
+		if (!next_word)
+		{
+			ft_free_result(result, i);
+			return (NULL);
+		}
+		result[i] = next_word;
+		i++;
+		start += len;
+	}
+	result[i] = NULL;
+	return (result);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		words;
+	char	**result;
+
+	if (!s)
+	{
+		return (NULL);
+	}
+	words = ft_countword(s, c);
+	result = (char **)malloc(sizeof(char *) * (words + 1));
+	if (!result)
+		return (NULL);
+	result = ft_fill_result(result, s, c, words);
+	return (result);
+}
+
+/* int	main(void)
+{
+	char	**result;
+	char	*str = "a";
+	char	delimiter = 'a';
+	int		i;
+
+	printf("Cadena original: \"%s\"\n", str);
+
+	// Llamamos a ft_split con la cadena y el delimitador
+	result = ft_split(str, delimiter);
+
+	if (!result)
+	{
+		printf("Error: ft_split devolvio NULL.\n");
+		return (1);
+	}
+
+	printf("Resultado de ft_split:\n");
+	i = 0;
+	while (result[i])
+	{
+		printf("[%d]: \"%s\"\n", i, result[i]);
+		free(result[i]);
+		i++;
+	}
+	return (0);
+} */
